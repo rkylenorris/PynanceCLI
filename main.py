2637120ec2bd38847f08a64817305171f423de90
Kyle Norris <r.kyle.norris@gmail.com>
2024-10-21 15:38:24 -0400

    updated clases to work with exporting to json, tested  in main

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # simple python finance tracker
 # author: R. Kyle Norris
-pass
+from tracker import Transaction, TransactionType, Pynance
+from datetime import datetime
+from random import randint
+
+if __name__ == '__main__':
+    tracker = Pynance()
+
+    for expense in ['groceries', 'rent', 'utilities']:
+        amount = randint(75, 600)
+        transaction = Transaction(amount=amount, cat=expense)
+        tracker.perform_transaction(transaction)
+
+    tracker.perform_transaction(Transaction(amount=2000,
+                                            transaction_type=TransactionType.income,
+                                            cat="salary",
+                                            desc="biweekly salary"))
+
+    print("\n-----view transactions-----")
+    tracker.view_transactions()
+
+    print("\n-----view summary-----")
+    tracker.view_summary()
+
+    print("\n-----end------")
--- tracker.py
+++ tracker.py
@@ -1,28 +1,38 @@
 import json
 from datetime import datetime
-from dataclasses import dataclass
+# from dataclasses import dataclass
+# from dataclasses_json import dataclass_json
 from enum import Enum
 
 
-class TransactionTypes(Enum):
+class TransactionType(Enum):
     expense = 0
     income = 1
 
 
-@dataclass
 class Transaction:
-    amount: float = 0.00
-    type: TransactionTypes = TransactionTypes.expense
-    category: str = ""
-    description: str = ""
-    transaction_datetime: datetime = datetime.now()
+    def __init__(self, amount: float, timestamp: datetime = datetime.now(),
+                 transaction_type: TransactionType = TransactionType.expense,
+                 cat: str = "", desc: str = ""):
+        self.amount: float = amount
+        self.creation_datetime = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        self.transaction_type = transaction_type.value
+        self.category = cat
+        self.description = desc
+
+    def __repr__(self):
+        return (f"Transaction:\t${self.amount} | {TransactionType(self.transaction_type).name} " +
+                f"| {self.category} | {self.description} | " +
+                f"{self.creation_datetime}")
 
 
 class Pynance:
     # python based finance tracker
     def __init__(self, data_file="data.json"):
         self.data_file = data_file
-        self.data = self.load_data()
+        data = self.load_data()
+        self.transactions = data.get("transactions")
+        self.total = data.get("total")
 
     def load_data(self):
         try:
@@ -36,19 +46,40 @@ class Pynance:
 
     def save_data(self):
         with open(self.data_file, "w") as f:
-            json.dump(self.data, f, indent=4)
+            json.dump(self.__dict__, f, indent=4)
 
     def perform_transaction(self, transaction: Transaction):
 
-        self.data['transactions'].append(transaction)
+        self.transactions.append(transaction.__dict__)
 
-        if transaction.type == TransactionTypes.expense:
-            self.data['total'] -= transaction.amount
-        elif transaction.type == TransactionTypes.income:
-            self.data['total'] += transaction.amount
+        if transaction.transaction_type == TransactionType.expense.value:
+            self.total -= transaction.amount
+        elif transaction.transaction_type == TransactionType.income.value:
+            self.total += transaction.amount
 
         self.save_data()
 
+    def view_transactions(self):
+        print("  \tAmount | Datetime | Type | Category | Description")
+        for i, tran in enumerate(self.transactions):
+            counter = i + 1
+            print(f"{counter}:\t{tran['amount']} | "
+                  f"{tran['creation_datetime']} | "
+                  f"{TransactionType(tran['transaction_type']).name} | "
+                  f"{tran['category']} | "
+                  f"{tran['description']}")
 
-
-
+    def view_summary(self):
+        income = [trans['amount'] for trans in self.transactions if
+                  trans['transaction_type'] == TransactionType.income.value]
+        expense = [trans['amount'] for trans in self.transactions if
+                   trans['transaction_type'] == TransactionType.expense.value]
+        total_income = sum(income)
+        count_income = len(income)
+        total_expense = sum(expense)
+        count_expense = len(expense)
+        print("----------SUMMARY----------")
+        print(f"Total Income: ${total_income} - Count: {count_income}")
+        print(f"Total Expense: ${total_expense} - Count: {count_expense}")
+        print(f'Current Balance: ${self.total}')
+        print("----------END SUMMARY----------")
